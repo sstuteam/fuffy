210636fb6f3d081e709e932ada131a03966564d3
trigger10 <ismailovanargilya@gmail.com>
2015-12-06 15:58:07 +0300

    Add admin panel

---
+++ Admin-panel/Scripts/my-script.js
@@ -0,0 +1,18 @@
+function changeBackground1(str) {
+    document.body.style.backgroundImage = "url('Styles/Images/symphony.png')";
+}
+
+function changeBackground2() {
+    document.body.style.backgroundImage = "url('Styles/Images/skulls.png')";
+}
+
+function changeBackground3() {
+    document.body.style.backgroundImage = "url('Styles/Images/pink_rice.png')";
+}
+
+function changeBackground4() {
+    document.body.style.backgroundImage = "url('Styles/Images/restaurant_icons.png')";
+}
+function changeBackground5() {
+    document.body.style.backgroundImage = "url('Styles/Images/congruent_pentagon.png')";
+}
