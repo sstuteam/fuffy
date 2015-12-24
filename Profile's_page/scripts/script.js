c4cf91ef93f4fc9280b9032af7f0c14f71962c8c
trigger10 <ismailovanargilya@gmail.com>
2015-12-25 00:08:16 +0300

    Profile's page (3)

--- Profile's_page/scripts/script.js
+++ Profile's_page/scripts/script.js
@@ -1,42 +1,64 @@
 var left=0;
 var right=0;
-function hideLeftDiv(){
+var like=0;
+var yhl=0;
+var width = window.width;
+$('#hide-profile').click(function(){
+
 	$('.content-cell-flank-left').hide();
-	$('.col-xs-6').removeClass('col-xs-6').addClass('col-xs-9');
+	$('.col-sm-9').removeClass('col-sm-9').addClass('col-sm-12');
 	if (right!=0){
-		$('.col-xs-9').removeClass('col-xs-9').addClass('col-xs-12');
+		$('.col-md-9').removeClass('col-md-9').addClass('col-md-12');
+	}
+	if (right==0){
+		$('.col-md-6').removeClass('col-md-6').addClass('col-md-9');
 	}
 	left=1;
-}
-function hideRightDiv(){
+})
+$('#hide-albums').click(function(){
 	$('.content-cell-flank-right').hide();
-	$('.col-xs-6').removeClass('col-xs-6').addClass('col-xs-9');
-	if (left!=0){
-		$('.col-xs-9').removeClass('col-xs-9').addClass('col-xs-12');
+	if (left==1){
+		$('.col-md-9').removeClass('col-md-9').addClass('col-md-12');
+		
+	}
+	if (left==0){
+		$('.col-md-6').removeClass('col-md-6').addClass('col-md-9');
 	}
 	right=1;
-}
-function showLeftDiv(){
+})
+$('#profile').click(function(){
 	$('.content-cell-flank-left').show();
+	$('.col-sm-12').removeClass('col-sm-12').addClass('col-sm-9');
 	if (left==1 && right==1){
-		$('.col-xs-12').removeClass('col-xs-12').addClass('col-xs-9');
+		$('.col-md-12').removeClass('col-md-12').addClass('col-md-9');
 	}
 	if (left==1 && right==0)
 	{
-		$('.col-xs-9').removeClass('col-xs-9').addClass('col-xs-6');
-	}
-	left=0;
-
-}
-function showRightDiv(){
-	$('.content-cell-flank-right').show();
-	if (left==1 && right==1){
-		$('.col-xs-12').removeClass('col-xs-12').addClass('col-xs-9');
+	    $('.col-md-9').removeClass('col-md-9').addClass('col-md-6');
 	}
 	if (left==0 && right==1)
 	{
-		$('.col-xs-9').removeClass('col-xs-9').addClass('col-xs-6');
+	    $('.col-md-9').removeClass('col-md-9').addClass('col-md-6');
+	}
+	left=0;
+
+})
+$('#albums').click(function(){
+	$('.content-cell-flank-right').show();
+	if (left==1 && right==1){
+		$('.col-md-12').removeClass('col-md-12').addClass('col-md-9');
+	}
+	if (left==0 && right==1)
+	{
+		$('.col-md-9').removeClass('col-md-9').addClass('col-md-6');
 	}
 	right=0;
+})
 
+function counter(){
+	if(yhl==0){
+		like=like+1;
+		$('.like').text(like);
+	}
+	yhl=1;
 }
