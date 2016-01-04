075a92eaeae88171f5815ba66412c8d35da60235
Eugene Sharlay <youjein100@mail.ru>
2016-01-04 11:17:43 +0300

    Link to DB, fixed Registration, added first User: eugene100, pass: 123, email: youjein100@mail.ru, Nick: Jericho, ID: Grid; New ConnectionString to FuffyDB

--- [SSTU]fuffy_v.1.1/DAL/DBWork.cs
+++ [SSTU]fuffy_v.1.1/DAL/DBWork.cs
@@ -21,11 +21,11 @@ namespace DAL
         {
             using (SqlConnection c = new SqlConnection(ConnectionString))
             {
-                SqlCommand com = new SqlCommand("INSERT INTO [dbo].[Profile] ([UserID],[Login],[Password],[Name],[Email]) VALUES (@UserID,@Login,@Password,@Name,@Email)", c);
-                com.Parameters.AddWithValue("@UserID", user.idUser);
+                SqlCommand com = new SqlCommand("INSERT INTO [dbo].[Login] ([ID],[Login],[Password],[Nick],[Email]) VALUES (@ID,@Login,@Password,@Nick,@Email)", c);
+                com.Parameters.AddWithValue("@ID", user.idUser);
                 com.Parameters.AddWithValue("@Login", user.Login);
                 com.Parameters.AddWithValue("@Password", user.Password);
-                com.Parameters.AddWithValue("@Name", user.Name);
+                com.Parameters.AddWithValue("@Nick", user.Name);
                 com.Parameters.AddWithValue("@Email", user.Email);
                 c.Open();
                 var a = com.ExecuteNonQuery();
@@ -38,12 +38,12 @@ namespace DAL
             var listUser = new List<User>();          
             using (SqlConnection c = new SqlConnection(ConnectionString))
             {
-                SqlCommand com = new SqlCommand("SELECT [UserID],[Login],[Password],[Email],[Name] FROM [dbo].[Profile]", c);
+                SqlCommand com = new SqlCommand("SELECT [ID],[Login],[Password],[Email],[Nick] FROM [dbo].[Login]", c);
                 c.Open();
                 var reader = com.ExecuteReader();
                 while (reader.Read())
                 {
-                    User user = new User((Guid)reader["UserID"],(string)reader["Login"], (string)reader["Password"], (string)reader["Name"],(string)reader["Email"]);
+                    User user = new User((Guid)reader["ID"],(string)reader["Login"], (string)reader["Password"], (string)reader["Nick"],(string)reader["Email"]);
                     listUser.Add(user);
                 }
             }
