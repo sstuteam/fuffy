fc15dbce4f9fef0826bffe0e3e14baf3a480b8d3
Alyona <mishckina.alyona808@live.com>
2016-01-14 23:53:23 +0300

    update pages and remove non-working version [SSTU]fuffy

---
+++ Pages/Scripts/admin-panel-change-background.js
@@ -0,0 +1,20 @@
+$('#changeBackground1').click(function () {
+    document.body.style.backgroundImage = "url('Content/symphony.png')";
+}
+)
+$('#changeBackground2').click(function () {
+    document.body.style.backgroundImage = "url('Content/skulls.png')";
+}
+)
+$('#changeBackground3').click(function () {
+    document.body.style.backgroundImage = "url('Content/pink_rice.png')";
+}
+)
+$('#changeBackground4').click(function () {
+    document.body.style.backgroundImage = "url('Content/restaurant_icons.png')";
+}
+)
+$('#changeBackground5').click(function () {
+    document.body.style.backgroundImage = "url('Content/congruent_pentagon.png')";
+}
+)
