ef45e1911de450b5e39cf5954479691a6daf2913
trigger10 <ismailovanargilya@gmail.com>
2016-04-09 17:56:31 +0300

    this must work

---
+++ [SSTU]fuffy_v.1.1/PL_MVC/Controllers/SettingsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL_MVC.Controllers
+{
+    public class SettingsController : Controller
+    {
+        // GET: Settings
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
