fb24085d778c8810039f43b4a7e0a56a8ca284a3
Eugene Sharlay <youjein100@mail.ru>
2016-04-11 12:57:18 +0300

    Added functions for Likes: GetLikes(ID)(Photo or comment), AddLike(ID)(Photo or comment),DeleteLike(ID)(Photo or comment)

--- [SSTU]fuffy_v.1.1/BLL/IBLL.cs
+++ [SSTU]fuffy_v.1.1/BLL/IBLL.cs
@@ -22,6 +22,9 @@ namespace BLL
         IEnumerable<Comment> GetComments();
         bool AddComment(Comment comment);
         bool CheckAlbumName(Guid Id, string Name);
+        int GetLikes(Guid Id);
+        bool AddLike(Guid Id);
+        bool DeleteLike(Guid Id);
         //пользователи
         bool AddUser(User user);
         bool CheckLogin(string Login);
