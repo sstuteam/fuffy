17e7162113592d3af0eb4f363820f2e0a1128410
trigger10 <ismailovanargilya@gmail.com>
2016-05-14 13:49:34 +0300

    background images

---
+++ [SSTU]fuffy_v.1.1/PL_MVC/Scripts/ajax.js
@@ -0,0 +1,46 @@
+﻿//$(document.body).on("click", "#news", function (e) {
+//    e.preventDefault();
+//    $.ajax({
+//        url: '/News/News',
+//        data: null,
+//        type: 'post',
+//        success: function (result) {
+//            $('body').empty().append(result);
+
+//        }
+//    })
+//})
+//$(document.body).on("click", "#people", function (e) {
+//    e.preventDefault();
+//    $.ajax({
+//        url: '/People/People',
+//        data: null,
+//        type: 'post',
+//        success: function (result) {
+//            $('body').empty().append(result);
+
+//        }
+//    })
+//})
+//$(document.body).on("click", "#settings", function (e) {
+//    e.preventDefault();
+//    $.ajax({
+//        url: '/Settings/Settings',
+//        data: null,
+//        type: 'post',
+//        success: function (result) {
+//            $('body').empty().append(result);
+//        }
+//    })
+//})
+//$(document.body).on("click", "#profile", function (e) {
+//    e.preventDefault();
+//    $.ajax({
+//        url: '/User/Profile',
+//        data: null,
+//        type: 'post',
+//        success: function (result) {
+//            $('body').empty().append(result);
+//        }
+//    })
+//})
---
+++ [SSTU]fuffy_v.1.1/PL_MVC/Scripts/chagneDesign.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="jquery.validate.js" />
+/// <reference path="jquery.validate.unobtrusive.min.js" />
+var prf = $('.preference').html();
+if (prf == "sport                                             ")
+{
+    document.body.style.backgroundImage = 'url(../Content/Photo/sport.png)';
+    $('.navbar').css("backgroundColor", "rgb(70, 54, 111)");
+    $('.navbar').css("border-color", "rgb(70, 54, 111)");
+}
+if (prf == "culture                                           ") {
+    document.body.style.backgroundImage = 'url(../Content/Photo/culture.jpg)';
+    $('.navbar').css("backgroundColor", "rgb(40, 113, 146)");
+    $('.navbar').css("border-color", "rgb(40, 113, 146)");
+}
+if (prf == "science                                           ") {
+    document.body.style.backgroundImage = 'url(../Content/Photo/science.jpg)';
+    $('.navbar').css("backgroundColor", "rgb(31, 31, 31)");
+    $('.navbar').css("border-color", "rgb(31, 31, 31)");
+}
+if (prf == "policy                                            ") {
+    document.body.style.backgroundImage = 'url(../Content/Photo/policy.jpg)';
+    $('.navbar').css("backgroundColor", "#288470");
+    $('.navbar').css("border-color", "#288470");
+}
